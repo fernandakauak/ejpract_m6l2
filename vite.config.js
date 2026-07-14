1209f8e6c77e1d6d900a1530cbab90e258f881ea
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-14 10:54:35 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
-
-
-
-
-export default defineConfig({
-  plugins: [vue()],
   base: '/ejpract_m6l2/',
 })
